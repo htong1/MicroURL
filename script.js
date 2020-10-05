106f8d5610336a1a5d84aecaaf651902243e7b39
htong1 <htong0001@gmail.com>
2020-10-05 02:57:51 +0000

    Will return the same short url if the inputted url is the same.

--- script.js
+++ script.js
@@ -1,40 +1,43 @@
-function shorten(){
-let url = document.getElementById("url").value;
-let result = document.getElementById("microURL");
-let token = map(url);
-result.innerHTML = makeMURL(token);
+function shorten() {
+    let url = document.getElementById("url").value;
+    let result = document.getElementById("microURL");
+    let token = map(url);
+    result.innerHTML = makeMURL(token);
 }
 
-function createToken(){
-let charMap = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-let token = "";
-for(let i = 0; i < 6; i++){
-  token += charMap[Math.floor(Math.random()*62)];
-}
-return token;
-}
-
-function map(url){
-let token = createToken();
-if(localStorage.getItem(token)){
-  if(url != localStorage.getItem(token)){
-     map();
-  }
-} else {
-  localStorage.setItem(token, url);
-  return token;
-}
+function createToken() {
+    let charMap = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let token = "";
+    for (let i = 0; i < 6; i++) {
+        token += charMap[Math.floor(Math.random() * 62)];
+    }
+    return token;
 }
 
-function makeMURL(token){
-  let mURL = window.location.protocol + "//" + window.location.host + window.location.pathname + "?" + token;
-  return mURL;
+function map(url) {
+    let token = createToken();
+    if (localStorage.getItem(url)) {
+        return localStorage.getItem(url);
+    } else if (localStorage.getItem(token)) {
+        if (url != localStorage.getItem(token)) {
+            map();
+        }
+    } else {
+        localStorage.setItem(token, url);
+        localStorage.setItem(url, token);
+        return token;
+    }
 }
 
-function extractToken(){
-  debugger;
-  let extractedToken = window.location.search;
-  extractedToken = extractedToken.slice(1);
-  let open = localStorage.getItem(extractedToken);
-  window.open(open);
+function makeMURL(token) {
+    let mURL = window.location.protocol + "//" + window.location.host + window.location.pathname + "?" + token;
+    return mURL;
+}
+
+function extractToken() {
+    debugger;
+    let extractedToken = window.location.search;
+    extractedToken = extractedToken.slice(1);
+    let open = localStorage.getItem(extractedToken);
+    window.open(open, "_parent");
 }
